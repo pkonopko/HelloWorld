07d1017ebeebd1853d7c32a2b99d71be05aea505
pkonopko <p.konopko99@gmail.com>
2024-07-21 14:54:40 +0200

    created Spring Boot project

---
+++ src/main/java/com/example/ShortnerURL/ShortnerUrlApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShortnerURL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortnerUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortnerUrlApplication.class, args);
+	}
+
+}
