5b8b1ff2d793da3c76a0a8197a46b7b467994324
pkonopko <p.konopko99@gmail.com>
2024-07-24 14:58:50 +0200

    Reapply "Setup postgres server locally, make the app connect to postgres"
    This reverts commit f95e9e714988f1e153804f9edaee86c2ddb053a3.

--- src/main/java/com/example/ShortnerURL/Controller/PingController.java
+++ src/main/java/com/example/ShortnerURL/Controller/PingController.java
@@ -1,12 +1,30 @@
 package com.example.ShortnerURL.Controller;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class PingController {
-    @GetMapping("/{ping}")
-    public String ping(){
-        return "ok";
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping("/ping")
+    public ResponseEntity<Map<String, String>> ping() {
+        try {
+            jdbcTemplate.queryForObject("SELECT current_timestamp;", String.class);
+            Map<String, String> response = new HashMap<>();
+            response.put("status", "ok");
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            Map<String, String> response = new HashMap<>();
+            response.put("status", "error");
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
