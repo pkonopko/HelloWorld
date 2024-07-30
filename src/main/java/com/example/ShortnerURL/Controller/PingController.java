e30b707a77307fd4726692324402499cf56ff1ba
pkonopko <p.konopko99@gmail.com>
2024-07-30 12:38:30 +0200

    change PingController, add Exceptions

--- src/main/java/com/example/ShortnerURL/Controller/PingController.java
+++ src/main/java/com/example/ShortnerURL/Controller/PingController.java
@@ -1,15 +1,12 @@
 package com.example.ShortnerURL.Controller;
-import com.example.ShortnerURL.WebDto.ErrorResponseDto;
+import com.example.ShortnerURL.Exceptions.ApplicationExceptions;
 import com.example.ShortnerURL.WebDto.SuccessResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class PingController {
@@ -17,18 +14,14 @@ public class PingController {
     private JdbcTemplate jdbcTemplate;
 
     @GetMapping("/ping")
-    public ResponseEntity<Object> ping() {
+    public SuccessResponseDto ping() {
         try {
             jdbcTemplate.queryForObject("SELECT current_timestamp;", String.class);
-            SuccessResponseDto successResponseDto = SuccessResponseDto.builder()
+            return SuccessResponseDto.builder()
                     .status("ok")
                     .build();
-            return new ResponseEntity<>(successResponseDto, HttpStatus.OK);
         } catch (Exception e) {
-            ErrorResponseDto errorResponseDto = ErrorResponseDto.builder()
-                    .status("error")
-                    .build();
-            return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+            throw new ApplicationExceptions("error", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
